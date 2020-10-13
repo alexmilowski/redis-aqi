98b9b47fd03072a0e827b9e21d600b085bcb8a00
Alex Milowski <alex@milowski.com>
2020-10-13 10:58:11 -0700

    added secondary navigation

--- docs/site.js
+++ docs/site.js
@@ -4,14 +4,31 @@ function setup() {
       setTimeout(setup,10);
       return
    }
+   if (document.location.pathname.endsWith("/")) {
+      let classes = document.body.getAttribute("class");
+      if (classes==null) {
+         classes = "";
+      }
+      classes += " main";
+      document.body.setAttribute("class",classes)
+   }
+   secondary = document.getElementById("secondary")
    for (let section of document.getElementsByClassName("tile")) {
       if (section.tagName!="SECTION") {
          continue;
       }
+      let a = section.getElementsByTagName('a')[0]
       section.addEventListener("click", () => {
-         let a = section.getElementsByTagName('a')[0]
          document.location = a.href;
       });
+      // let title = section.getElementsByClassName("tile")[0];
+      // let container = document.createElement("span");
+      // container.setAttribute("class","item");
+      // let link = document.createElement("a");
+      // link.setAttribute("href",a.getAttribute("href"));
+      // link.appendChild(document.createTextNode(title.textContent));
+      // container.appendChild(link);
+      // secondary.appendChild(container);
    }
 }
 
